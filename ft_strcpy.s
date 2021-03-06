59c3df921eed1e2d52b19f0c818d052f1dd38604
Abdelaziz Bel haj <abel-haj@e1r2p8.1337.ma>
2021-03-06 19:33:54 +0100

    backup

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -1,11 +1,34 @@
 section .data
 	msg db "string",0
+
 section .text
 	global _ft_strcpy
 
+;	rdi , rsi
+;	dest, src 
 _ft_strcpy:
-	extern _ft_strlen
-	mov rdi, msg
-	call _ft_strlen
-	; mov rax, 10
+	xor rax, rax
+	cmp byte [rsi + rax], 0
+
+loop_it:
+	je end_it
+	; movsx rdi, byte [rsi + rax]
+	mov dil, [rsi + rax]
+	mov [rdi + rax], dil
+	inc rax
+	jmp loop_it
+
+end_it:
+	; movsx rdi, byte [rsi + rax]
+	mov dil, [rsi + rax]
+	mov [rdi + rax], dil
+	mov rax, rdi
 	ret
+
+	; extern _ft_strlen
+	; mov rdi, msg
+	; call _ft_strlen
+
+	; mov rax, 10
+
+	; ret
--- ft_strlen.s
+++ ft_strlen.s
@@ -5,6 +5,11 @@ _ft_strlen:
 	; zeroing rax
 	xor rax, rax
 
+; FOR DEBUGGING
+	cmp rdi, 0
+	je error_it
+; FOR DEBUGGING
+
 loop_it:
 	; if specified byte equals '\0'
 	cmp byte [rdi + rax], 0
@@ -18,3 +23,9 @@ loop_it:
 return_it:
 	; exit everything
 	ret
+
+; FOR DEBUGGING
+error_it:
+	mov rax, -1
+	ret
+; FOR DEBUGGING
