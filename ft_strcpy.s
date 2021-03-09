4051d202ee1635979388721dbc1d61da86f62f56
Abdelaziz Bel haj <abel-haj@e1r2p8.1337.ma>
2021-03-09 12:59:06 +0100

    and fiiiiiiiiinally, finished strcpy

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -1,34 +1,65 @@
-section .data
-	msg db "string",0
-
 section .text
 	global _ft_strcpy
 
-;	rdi , rsi
-;	dest, src 
+; 	rdi , rsi
+; 	dest, src
 _ft_strcpy:
-	xor rax, rax
-	cmp byte [rsi + rax], 0
-
-loop_it:
+	cmp rsi, 0
 	je end_it
-	; movsx rdi, byte [rsi + rax]
-	mov dil, [rsi + rax]
-	mov [rdi + rax], dil
+	cmp rdi, 0
+	je end_it
+
+	xor rax, rax
+	xor r9, r9
+	xor dil, dil
+loop_it:
+; 	cmp [rsi + rax], byte 0
+; 	mov dil, byte [rsi + rax]
+; 	mov [rdi + rax], dil
+; 	je end_it
+; 	inc rax
+; 	jmp loop_it
+
+	cmp  [rsi + rax], byte 0
+	movzx r9, byte [rsi + rax]
+	mov [rdi + rax], r9
+	je end_it
 	inc rax
 	jmp loop_it
 
+	; movzx r9, byte [rsi + 0]
+	; mov [rdi + 0], r9
+	; movzx r9, byte [rsi + 1]
+	; mov [rdi + 1], r9
+	; movzx r9, byte [rsi + 2]
+	; mov [rdi + 2], r9
+	; movzx r9, byte [rsi + 3]
+	; mov [rdi + 3], r9
+	; movzx r9, byte [rsi + 500]
+	; mov [rdi + 500], r9
+
 end_it:
-	; movsx rdi, byte [rsi + rax]
-	mov dil, [rsi + rax]
-	mov [rdi + rax], dil
 	mov rax, rdi
 	ret
 
-	; extern _ft_strlen
-	; mov rdi, msg
-	; call _ft_strlen
 
-	; mov rax, 10
+; _ft_strcpy:
+; 	cmp byte [rsi], 0
+; 	je end
+; loop:
+; 	mov dil, byte [rsi]
+; 	xor r9, r9
+; 	mov [rdi], r9
+; 	mov [rdi], dil
+; 	add rsi, 1
+; 	cmp byte [rsi], 0
+; 	je end
+; 	add rdi, 1
+; 	jmp loop
 
-	; ret
+; end:
+; 	add rdi, 1
+; 	xor r9, r9
+; 	mov [rdi], r9
+; 	mov rax, rdi
+; 	ret
