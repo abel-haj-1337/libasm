908c5f547f19cae5e333fc3dde76f03b4e14a82b
Abdelaziz Bel haj <abel-haj@e1r2p8.1337.ma>
2021-03-09 19:28:18 +0100

    return problem

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -4,12 +4,30 @@ section .text
 _ft_strcmp:
 	; initializing index
 	xor rax, rax
+	xor r9, r9
 
 loop_it:
-	cmp byte [rdi + rax], [rsi + rax]
-	je return_it
-	inc rax
+	movzx r9, byte [rdi]
+	cmp r9, [rsi]
+	jg greater_than
+	jb lower_than
+	cmp r9, byte 0
+	je equal_to
+	add rdi, 1
+	add rsi, 1
 	jmp loop_it
+
+equal_to:
+	mov rax, 0
+	jmp return_it
+
+greater_than:
+	mov rax, 1
+	jmp return_it
+
+lower_than:
+	mov rax, -1
+	jmp return_it
 
 return_it:
 	ret
