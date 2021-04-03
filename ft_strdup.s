0dc5f61b25645ba5337227bcb1cb879758380b11
Abdelaziz Bel haj <abel-haj@e1r2p8.1337.ma>
2021-04-03 18:22:35 +0100

    done duplicating

--- ft_strdup.s
+++ ft_strdup.s
@@ -2,13 +2,18 @@ section .text
 	global _ft_strdup
 	extern _malloc
 	extern _ft_strlen
+	extern ___error
 
 _ft_strdup:
-	add rdi, 3
-	mov rax, rdi
-	ret
+	; FOR TESTING
+	; add rdi, 3
+	; mov rax, rdi
+	; ret
+	; rax = 0 pop qword[056146546]
 
 	xor rax, rax
+	; rdi = "str"
+	; rdi = ?
 
 	call _ft_strlen
 
@@ -23,19 +28,29 @@ _ft_strdup:
 	mov r10, rax
 	call _malloc
 
+	; now rax holds allocated space
+	; and r10 holds string length
+
 	; retrieve strdup first argument
 	pop rdi
 
-; duplicating string
+	xor r9, r9
+
 loop_it:
-	xor r11, r11
-	mov r11b, [rdi]
-	mov [rax], r11b
-	cmp r11b, byte 0
+	cmp [rdi + r9], byte 0
+	movzx r11, byte [rdi + r9]
+	mov [rax + r9], r11
+	je return_it
+	inc r9
+	jmp loop_it
+
 	je return_it
 	add rdi, 1
 	add rax, 1
+
 	jne loop_it
+
+	call ___error
 
 return_it:
 	ret
